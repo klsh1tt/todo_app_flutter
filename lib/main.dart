8c835a4f2cab4f8738bac724d6912044217e10ba
klsh1tt <klsh1tt@gmail.com>
2022-04-02 22:08:23 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_flutter/presentation/todo_list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Todo App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BookListSample',
+      home: TodoListPage(),
+    );
+  }
+}
---
+++ lib/presentation/todo_list_page.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class TodoListPage extends StatelessWidget {
+  const TodoListPage({Key? key}) : super(key: key);
+}
+ 
